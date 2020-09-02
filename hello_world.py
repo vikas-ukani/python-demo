5c2965bdaef81cdd3090953396d89611afc7dc52
Vikas Ukani <57585690+vikas-ukani@users.noreply.github.com>
2020-09-02 15:12:33 +0530

    Update my practices with python scripts and update the demo code.

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,21 @@
-print("Hello World!! I'm vikas ukani from india. I'm working as a python developer 
-from python workshop.")
+
+# A Little information about me !!! 
+print("Hello World!! I'm vikas ukani from india. I'm working as a python developer from python workshop. This my initial python scripts and my little practicites.")
+
+print( "Work with numbers")
+
+price = 1.25
+margine = .5
+amount = 500
+
+
+final_total = price * margine
+print("The Final total of price and margine is :", final_total)
+
+my_number = 1
+favorite_number = 5
+nature_number = 3
+
+myName = "Vikas Ukani"
+
+print(f"Hello Everyone, I'm {} and i want to be {}st number with my practices", format(myName, my_number) )
